d86b92ef1592f4bb2c07ec709f5b25308e4b1127
Atharv Kulkarni <atharvkulkarni1211@gmail.com>
2022-07-14 23:10:02 +0530

    Day-13 Done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,12 +24,14 @@ class _HomepageState extends State<Homepage> {
     var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
     var decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -40,18 +42,23 @@ class _HomepageState extends State<Homepage> {
 
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            //index gives postion
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) =>
+                    //index gives postion
+                    ItemWidget(
+                  item: CatalogModel.items[index],
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ), //render items as the screen is scrolled
 
       drawer: MyDrawer(),
     );
   }
 }
+
+//null can be done in new flutter
